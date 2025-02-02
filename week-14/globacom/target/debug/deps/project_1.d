980445c2c3cb1fe0ea354637a84baf9895168bc2
Splinterz07 <chukwubuikem888@gmail.com>
2025-02-02 22:09:22 +0100

    My tenth commit

---
+++ week-14/globacom/target/debug/deps/project_1.d
@@ -0,0 +1,5 @@
+C:\Users\User PC\OneDrive\Documents\m.nwachukwuCSC101\week-14\globacom\target\debug\deps\project_1.exe: src/main.rs
+
+C:\Users\User PC\OneDrive\Documents\m.nwachukwuCSC101\week-14\globacom\target\debug\deps\project_1.d: src/main.rs
+
+src/main.rs:
---
+++ week-14/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\User\ PC\OneDrive\Documents\m.nwachukwuCSC101\week-14\project_1\target\debug\project_1.exe: C:\Users\User\ PC\OneDrive\Documents\m.nwachukwuCSC101\week-14\project_1\src\main.rs

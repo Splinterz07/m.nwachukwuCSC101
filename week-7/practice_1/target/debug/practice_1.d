8b37015b6c0fd12e19af2f6d2c4caafd509c2cb0
Splinterz07 <chukwubuikem888@gmail.com>
2025-02-02 23:53:24 +0100

    My sixth commit

---
+++ week-7/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\User\ PC\OneDrive\Documents\m.nwachukwuCSC101\week-7\practice_1\target\debug\practice_1.exe: C:\Users\User\ PC\OneDrive\Documents\m.nwachukwuCSC101\week-7\practice_1\src\main.rs
